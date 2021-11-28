36b9c58a34d355b8796b35a2aa64947346f6b0a9
carljhn <zoletacarljohn1@gmail.com>
2021-11-27 22:03:43 -1200

    Initial commit: I have added the pseudocode for this prog and added the variable for the input sentence

---
+++ charCount.py
@@ -0,0 +1,12 @@
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
+
+sntnc=input("What do you want to say?: ")
+sntnc_low=sntnc.lower() # convert the words to lowercase
+
